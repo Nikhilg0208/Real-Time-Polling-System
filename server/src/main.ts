461eb28165725358b0c96f9786aac450e112acbe
Nikhilg0208 <nikhilg1098@gmail.com>
2025-08-12 15:35:15 +0530

    set up completed

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,29 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import morgan from 'morgan';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS
+  app.enableCors({
+    origin: 'http://localhost:5173/', // your frontend URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // allow cookies if needed
+  });
+
+  // Use morgan only in development mode
+  if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+  }
+
+  // Global validation
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Global prefix
+  app.setGlobalPrefix('api/v1');
+
+  await app.listen(process.env.PORT || 4200);
 }
 bootstrap();
